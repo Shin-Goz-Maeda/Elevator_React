b3c40b5c7f9bf3e496a1ece9ddce571077cac931
Shin <niconico@Shinsuke-MacBook.local>
2022-12-28 01:19:54 +0900

    エレベーターがフロアを動く処理を追加

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Elevator } from './elevator/elevator';
 import { Display } from './Display/Display';
 import { Btns } from './Btn/btns';
 import styled from 'styled-components';
 
 const Content = () => {
-  const [ onColor, setOnColor ] = useState([1,1]);
+  const [ onColor, setOnColor ] = useState([1]);
   const [ witchBtn, setWitchBtn ] = useState("up");
 
   const [ floor1 , setFloor1 ] = useState(false);
@@ -18,11 +18,13 @@ const Content = () => {
 
   console.log(onColor);
 
+
+
   const moveElevator = () => {
     const onElevator = onColor.slice(-2)[0];
     const goingElevator = onColor.slice(-1)[0];
-
-    console.log(goingElevator);
+    console.log(onElevator+"on");
+    console.log(goingElevator+"going");
     if (onElevator === 5 && goingElevator === 1) {
       setFloor5(true);
       setTimeout(() => {
@@ -71,14 +73,173 @@ const Content = () => {
         setFloor5(false);
         setFloor4(true);
       }, 1000);
+
+    } else if (onElevator === 4 && goingElevator === 1) {
+      setFloor4(true);
+      setTimeout(() => {
+        setFloor4(false);
+        setFloor3(true);
+      }, 1000);
+      setTimeout(() => {
+        setFloor3(false);
+        setFloor2(true);
+      }, 2000);
+      setTimeout(() => {
+        setFloor2(false);
+        setFloor1(true);
+      }, 3000);
+    } else if (onElevator === 4 && goingElevator === 2) {
+      setFloor4(true);
+      setTimeout(() => {
+        setFloor4(false);
+        setFloor3(true);
+      }, 1000);
+      setTimeout(() => {
+        setFloor3(false);
+        setFloor2(true);
+      }, 2000);
+    } else if (onElevator === 4 && goingElevator === 3) {
+      setFloor4(true);
+      setTimeout(() => {
+        setFloor4(false);
+        setFloor3(true);
+      }, 1000);
+    } else if (onElevator === 4 && goingElevator === 5) {
+      setFloor4(true);
+      setTimeout(() => {
+        setFloor4(false);
+        setFloor5(true);
+      }, 1000);
+
+    } else if (onElevator === 3 && goingElevator === 1) {
+      setFloor3(true);
+      setTimeout(() => {
+        setFloor3(false);
+        setFloor2(true);
+      }, 1000);
+      setTimeout(() => {
+        setFloor2(false);
+        setFloor1(true);
+      }, 2000);
+    } else if (onElevator === 3 && goingElevator === 2) {
+      setFloor3(true);
+      setTimeout(() => {
+        setFloor3(false);
+        setFloor2(true);
+      }, 1000);
+    } else if (onElevator === 3 && goingElevator === 4) {
+      setFloor3(true);
+      setTimeout(() => {
+        setFloor3(false);
+        setFloor4(true);
+      }, 1000);
+    } else if (onElevator === 3 && goingElevator === 5) {
+      setFloor3(true);
+      setTimeout(() => {
+        setFloor3(false);
+        setFloor4(true);
+      }, 1000);
+      setTimeout(() => {
+        setFloor4(false);
+        setFloor5(true);
+      }, 2000);
+
+    } else if (onElevator === 2 && goingElevator === 1) {
+      setFloor2(true);
+      setTimeout(() => {
+        setFloor2(false);
+        setFloor1(true);
+      }, 1000);
+    } else if (onElevator === 2 && goingElevator === 3) {
+      setFloor2(true);
+      setTimeout(() => {
+        setFloor2(false);
+        setFloor3(true);
+      }, 1000);
+    } else if (onElevator === 2 && goingElevator === 4) {
+      setFloor2(true);
+      setTimeout(() => {
+        setFloor2(false);
+        setFloor3(true);
+      }, 1000);
+      setTimeout(() => {
+        setFloor3(false);
+        setFloor4(true);
+      }, 2000);
+    } else if (onElevator === 2 && goingElevator === 5) {
+      setFloor2(true);
+      setTimeout(() => {
+        setFloor2(false);
+        setFloor3(true);
+      }, 1000);
+      setTimeout(() => {
+        setFloor3(false);
+        setFloor4(true);
+      }, 2000);
+      setTimeout(() => {
+        setFloor4(false);
+        setFloor5(true);
+      }, 3000);
+
+    } else if (onElevator === 1 && goingElevator === 2) {
+      setFloor1(true);
+      setTimeout(() => {
+        setFloor1(false);
+        setFloor2(true);
+      }, 1000);
+    } else if (onElevator === 1 && goingElevator === 3) {
+      setFloor1(true);
+      setTimeout(() => {
+        setFloor1(false);
+        setFloor2(true);
+      }, 1000);
+      setTimeout(() => {
+        setFloor2(false);
+        setFloor3(true);
+      }, 2000);
+    } else if (onElevator === 1 && goingElevator === 4) {
+      setFloor1(true);
+      setTimeout(() => {
+        setFloor1(false);
+        setFloor2(true);
+      }, 1000);
+      setTimeout(() => {
+        setFloor2(false);
+        setFloor3(true);
+      }, 2000);
+      setTimeout(() => {
+        setFloor3(false);
+        setFloor4(true);
+      }, 3000);
+    } else if (onElevator === 1 && goingElevator === 5) {
+      setFloor1(true);
+      setTimeout(() => {
+        setFloor1(false);
+        setFloor2(true);
+      }, 1000);
+      setTimeout(() => {
+        setFloor2(false);
+        setFloor3(true);
+      }, 2000);
+      setTimeout(() => {
+        setFloor3(false);
+        setFloor4(true);
+      }, 3000);
+      setTimeout(() => {
+        setFloor4(false);
+        setFloor5(true);
+      }, 4000);
     }
-  };
+  }
 
   const toggle = (a, b) => {
     setOnColor([...onColor, a]);
     setWitchBtn(b);
-    moveElevator();
   };
+
+  useEffect(() => {
+    moveElevator();
+  }, [onColor])
 
   return (
     <>
